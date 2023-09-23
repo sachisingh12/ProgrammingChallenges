f1ce5c2736df7d0bd16db7caeb6cddc1a317d098
sachisingh12 <145872124+sachisingh12@users.noreply.github.com>
2023-09-23 13:30:54 -0700

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -8,4 +8,29 @@ GROUP BY DATE(date);
 
 /* Question 2 */
 
+SELECT state, SUM(revenue) AS total_revenue
+FROM website_revenue
+GROUP BY state
+ORDER BY total_revenue DESC
+LIMIT 3;
+
+
+/* Question 3 */
+
+SELECT campaign_id,
+       SUM(cost) AS total_cost,
+       SUM(impressions) AS total_impressions,
+       SUM(clicks) AS total_clicks,
+       SUM(revenue) AS total_revenue
+FROM marketing_data
+GROUP BY campaign_id;
+
+/* Question 4 */
+
+SELECT state, SUM(conversions) AS total_conversions
+FROM marketing_data
+WHERE campaign_id = 'Campaign5'
+GROUP BY state
+ORDER BY total_conversions DESC
+LIMIT 1;
 
